3b1fab3e906fdfab6bb0d9e3d96fb5b90d14c7b4
Julio César Domínguez Giraldo <juliodominguez@bloomweb.co>
2013-01-17 18:09:20 -0500

    paginado productos acorde catalogo, categoria y subcategoria

--- app/Controller/CatalogsController.php
+++ app/Controller/CatalogsController.php
@@ -31,63 +31,39 @@ class CatalogsController extends AppController {
 		if (!$this -> Catalog -> exists()) {
 			throw new NotFoundException(__('Catálogo no válido'));
 		}
-		$this -> Session -> write('catalog',$id); // PARA CONTROL EN LAS VITAS
+		$this -> Session -> write('catalog',$id); // PARA CONTROL EN LAS VISTAS
 		$this -> set('catalog', $this -> Catalog -> read(null, $id));
 		
 		// PAGINADO DE PRODUCTOS
-		$conditions = array();
 		$this -> loadModel('Product');
 		$this -> Product -> contain();
-		if($this -> request -> is('post') || $this -> request -> is('put')) {
-			// Se envía categoría
-			if(!empty($this -> request -> data['Filtro']['categories']) && empty($this -> request -> data['Filtro']['subcategories'])) {
-				// Paginado
-				$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('conditions' => array('Subcategory.category_id' => $this -> request -> data['Filtro']['categories']), 'fields' => array('Subcategory.id')));
-				$products = $this -> Catalog -> Category -> Subcategory -> ProductsSubcategory -> find('list', array('conditions' => array('ProductsSubcategory.subcategory_id' => $subcategories), 'fields' => array('ProductsSubcategory.product_id')));
-				$conditions['Product.id'] = $products;
-				
-				// Filtros
-				$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.id' => $this -> request -> data['Filtro']['categories'])));
-				$this -> set('categories', $categories);
-				$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('conditions' => array('Subcategory.category_id' => $this -> request -> data['Filtro']['categories'])));
-				$this -> set('subcategories', $subcategories);
-			}
-			// Se envía subcategoría :: NO SE DA POR EL MOMENTO
-			elseif(empty($this -> request -> data['Filtro']['categories']) && !empty($this -> request -> data['Filtro']['subcategories'])) {
-				
-			}
-			// Se envía categoría y subcategoría
-			elseif(!empty($this -> request -> data['Filtro']['categories']) && !empty($this -> request -> data['Filtro']['subcategories'])) {
-				// Paginado
-				$products = $this -> Catalog -> Category -> Subcategory -> ProductsSubcategory -> find('list', array('conditions' => array('ProductsSubcategory.subcategory_id' => $this -> request -> data['Filtro']['subcategories']), 'fields' => array('ProductsSubcategory.product_id')));
-				$conditions['Product.id'] = $products;
-				
-				// Filtros
-				$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.id' => $this -> request -> data['Filtro']['categories'])));
-				$this -> set('categories', $categories);
-				$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('conditions' => array('Subcategory.category_id' => $this -> request -> data['Filtro']['categories'])));
-				$this -> set('subcategories', $subcategories);
-			}
-			// Se envía vacío
-			else {
-				$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.catalog_id' => $id)));
-				$this -> set('categories', $categories);
-				/*$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.catalog_id' => $id), 'fields' => array('Category.id')));
-				$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('conditions' => array('Subcategory.category_id' => $categories)));
-				$this -> set('subcategories', $subcategories);*/
-			}
-		} else {
-			$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.catalog_id' => $id)));
-			$this -> set('categories', $categories);
-			/*$categories = $this -> Catalog -> Category -> find('list', array('conditions' => array('Category.catalog_id' => $id), 'fields' => array('Category.id')));
-			$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('conditions' => array('Subcategory.category_id' => $categories)));
-			$this -> set('subcategories', $subcategories);*/
+		
+		$categories = array();
+		$subcategories = array();
+		$products = array();
+		
+		if(isset($this -> passedArgs['subcategory'])) {
+			//debug('subcategoria');
+			$categories[$this -> passedArgs['category']] = $this -> passedArgs['category'];
+			$subcategories[$this -> passedArgs['subcategory']] = $this -> passedArgs['subcategory'];
+		} elseif(isset($this -> passedArgs['category'])) {
+			//debug('categoria');
+			$categories[$this -> passedArgs['category']] = $this -> passedArgs['category'];
+			$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('fields' => array('Subcategory.id'), 'conditions' => array('Subcategory.category_id' => $categories)));
+		} elseif(isset($this -> passedArgs['0'])) {
+			//debug('catalogo');
+			$categories = $this -> Catalog -> Category -> find('list', array('fields' => array('Category.id'), 'conditions' => array('Category.catalog_id' => $this -> passedArgs['0'])));
+			$subcategories = $this -> Catalog -> Category -> Subcategory -> find('list', array('fields' => array('Subcategory.id'), 'conditions' => array('Subcategory.category_id' => $categories)));
 		}
+		
+		$products = $this -> Product -> ProductsSubcategory -> find('list', array('fields' => array('ProductsSubcategory.product_id'), 'conditions' => array('ProductsSubcategory.subcategory_id' => $subcategories)));
+		
 		$this -> paginate = array(
 			'Product' => array(
-				'conditions' => $conditions
+				'conditions' => array('Product.id' => $products)
 			)
 		);
+		
 		$this -> set('products', $this -> paginate('Product'));
 	}
 
